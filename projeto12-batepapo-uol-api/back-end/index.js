7715674078ba67a2da050d2200d91690094e6489
Driven Education <driven@pop-os.localdomain>
2022-09-03 17:45:34 -0300

    When the user inputs a user name, this one will be validate (must be a non-empty string and this user name cannot being used

---
+++ projeto12-batepapo-uol-api/back-end/index.js
@@ -0,0 +1,59 @@
+import express, {json} from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from "dotenv";
+import dayjs from 'dayjs';
+import cors from 'cors';
+import joi from 'joi';
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect(() => {
+    db = mongoClient.db('batepapoUOL');
+});
+
+const server = express();
+server.use(cors());
+server.use(json());
+
+const nameSchema = joi.string().empty().required();
+
+server.post("/participants", async (req,res) => {
+    
+    const {name} = await req.body;
+    const validation = nameSchema.validate(name, {abortEarly: false});
+
+    if(validation.error){
+        return res.sendStatus(422);
+    };
+
+    try{
+        
+        const participant = await db.collection('participants').findOne({name: name}); 
+
+        if(participant){
+            return res.sendStatus(409);
+        };
+        
+        await db.collection('participants').insertOne({name: name, lastStatus: Date.now()});
+
+        await db.collection('messages').insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...', 
+            type: 'status', 
+            time: dayjs().locale('pt-br').format('HH:mm:ss')
+        });
+
+        return res.sendStatus(201);
+
+    }catch(error){
+        console.error(error);
+        return res.sendStatus(500);
+    }
+
+});
+
+server.listen(5000);
